12e9b71ee7a05a395dfb91dfc1d0da4a81efb3b1
Masafumi Oyamada <stillpedant@gmail.com>
2018-01-01 23:56:29 +0900

    Simultaneous multiple devices support & automatic keyboard detection

--- pykeymacs/input.py
+++ pykeymacs/input.py
@@ -6,18 +6,28 @@ from .transform import on_event
 __author__ = 'zh'
 
 
+def get_devices_list():
+    return [InputDevice(device_fn) for device_fn in reversed(list_devices())]
+
+
+def is_keyboard_device(device):
+    capabilities = device.capabilities(verbose=True)
+    if ('EV_KEY', 1) not in capabilities:
+        return False
+    if ('KEY_SPACE', 57) not in capabilities[('EV_KEY', 1)]:
+        return False
+    return True
+
+
 def select_device():
     """Select a device from the list of accessible input devices."""
-
-    devices = [InputDevice(device_fn) for device_fn in reversed(list_devices())]
+    devices = get_devices_list()
     if not devices:
         print('error: no input devices found (do you have rw permission on /dev/input/*?)')
         exit(1)
 
-    # Filter out non-keyboard devices (TODO: Make it more robust)
-    import re
     devices = [device for device in devices
-               if re.search("keyboard", device.name, re.IGNORECASE)]
+               if is_keyboard_device(device) and device.name != "py-evdev-uinput"]
 
     device_format = '{0:<3} {1.fn:<20} {1.name:<35} {1.phys}'
     device_lines = [device_format.format(n, d) for n, d in enumerate(devices)]
@@ -27,24 +37,24 @@ def select_device():
     print('\n'.join(device_lines))
     print('')
 
-    if len(device_lines) == 1:
-        return devices[0]
-    else:
-        choice = input('Select device [0-{}]:'.format(len(device_lines) - 1))
-        return devices[int(choice)]
+    return devices
 
 
-def loop(device):
+def loop(devices):
     try:
-        device.grab()
+        for device in devices:
+            device.grab()
     except IOError:
         print("IOError when grabbing device")
         exit(1)
     try:
         while True:
-            select([device], [], [])
-            for event in device.read():
-                if event.type == ecodes.EV_KEY:
-                    on_event(event)
+            r, w, x = select(devices, [], [])
+            for device in r:
+                # device = devices[fd]
+                for event in device.read():
+                    if event.type == ecodes.EV_KEY:
+                        on_event(event)
     finally:
-        device.ungrab()
+        for device in devices:
+            device.ungrab()
