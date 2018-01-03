35bf2b2524e3a4c4cb8d5b732df9bfb5facebd55
Masafumi Oyamada <stillpedant@gmail.com>
2018-01-03 11:53:38 +0900

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name             = "xkeysnail",
-      version          = "0.0.3",
+      version          = "0.0.4",
       author           = "Masafumi Oyamada",
       url              = "https://github.com/mooz/xkeysnail",
       description      = "Yet another keyboard remapping tool for X environment.",
