fd3460552e3c4287aee6e07154d04f81bbfe892d
luizoti <luizlazoti@hotmail.com>
2021-07-15 17:19:00 -0300

    build: :heavy_plus_sign: add custon python-evdev repo install

--- setup.py
+++ setup.py
@@ -8,14 +8,23 @@ __description__ = None
 
 exec(open("xkeysnail/info.py").read())
 
-setup(name             = "xkeysnail",
-      version          = __version__,  # pylint: disable=undefined-variable
-      author           = "Masafumi Oyamada",
-      url              = "https://github.com/mooz/xkeysnail",
-      description      = __description__,  # pylint: disable=undefined-variable
-      long_description = __doc__,
-      packages         = ["xkeysnail"],
-      scripts          = ["bin/xkeysnail"],
-      license          = "GPL",
-      install_requires = ["evdev", "python-xlib", "inotify_simple", "psutil"]
-      )
+setup(
+    name="xkeysnail",
+    version=__version__,  # pylint: disable=undefined-variable
+    author="Masafumi Oyamada",
+    url="https://github.com/mooz/xkeysnail",
+    description=__description__,  # pylint: disable=undefined-variable
+    long_description=__doc__,
+    packages=["xkeysnail"],
+    scripts=["bin/xkeysnail"],
+    license="GPL",
+    dependency_links=[
+        "https://github.com/luizoti/python-evdev/tarball/master#egg=python-evdev-1.4.1",
+    ],
+    install_requires=[
+        "python-evdev==1.4.1",
+        "python-xlib",
+        "inotify_simple",
+        "psutil"
+    ],
+)
