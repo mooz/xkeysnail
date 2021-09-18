132f6da84d9c86330e978615b11a28b5e1e68cfd
luizoti <luizlazoti@hotmail.com>
2021-09-17 22:54:11 -0300

    update in error capture

--- xkeysnail/xlib_info.py
+++ xkeysnail/xlib_info.py
@@ -11,18 +11,23 @@ from xkeysnail.log import log_msg
 def get_window_info(window):
     """Get window's info (recursively checks parents)."""
     try:
-        wmname = window.get_wm_name()
-        wmclass = window.get_wm_class()[1]  # (process name, class name)
-    except (TypeError, AttributeError):
-        wmclass = window.get_wm_class()
+        try:
+            wmname = window.get_wm_name()
+            # (process name, class name)
+            wmclass = window.get_wm_class()[1]
+        except TypeError:
+            wmclass = window.get_wm_class()
+            pass
+        # workaround for Java app
+        # https://github.com/JetBrains/jdk8u_jdk/blob/master/src/solaris/classes/sun/awt/X11/XFocusProxyWindow.java#L35
+        if not wmclass and not wmname or "FocusProxy" in wmclass:
+            parent_window = window.query_tree().parent
+            if parent_window:
+                return get_window_info(parent_window)
+        return {'wmclass': str(wmclass), 'wmname': wmname}
+    except AttributeError:
         pass
-    # workaround for Java app
-    # https://github.com/JetBrains/jdk8u_jdk/blob/master/src/solaris/classes/sun/awt/X11/XFocusProxyWindow.java#L35
-    if not wmclass and not wmname or "FocusProxy" in wmclass:
-        parent_window = window.query_tree().parent
-        if parent_window:
-            return get_window_info(parent_window)
-    return {'wmclass': str(wmclass), 'wmname': wmname}
+    return {'wmclass': '', 'wmname': ''}
 
 
 def get_display(display=None, time=30):
